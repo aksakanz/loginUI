68851826a2d983567f2109dd66d03de2703ce3a9
Aksa kanz Daffa <53376441+aksakanz@users.noreply.github.com>
2021-05-01 14:08:45 +0700

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'login_page.dart';
+import 'home_page.dart';
+import 'regis_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  final routes = <String, WidgetBuilder>{
+    LoginPage.tag: (context) => LoginPage(),
+    HomePage.tag: (context) => HomePage(),
+    RegisPage.tag: (context) => RegisPage(),
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Kodeversitas',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.lightBlue,
+      ),
+      home: LoginPage(),
+      routes: routes,
+    );
+  }
+}
